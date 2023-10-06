eda6f02b9eb2c6c68d580925d569cd34131884eb
Azrael229 <israel.navarrete229@gmail.com>
2023-10-06 00:16:24 -0600

    BD hosaying

--- conexion.php
+++ conexion.php
@@ -1,7 +1,12 @@
 <?php
+// para conectar con la BD del hosting 
+// ('mysql:host=localhost;dbname=BD_solusoft', 'israelprogramador', '744920lovepass')
+
+// para conectar con la BD de localhost
+// ('mysql:host=localhost;dbname=gastos_bys', 'root', '')
 
 try{
-    $conexion = new PDO('mysql:host=localhost;dbname=gastos_bys', 'root', '');
+    $conexion = new PDO('mysql:host=localhost;dbname=BD_solusoft', 'israelprogramador', '744920lovepass');
     // echo "Conexión OK";
 }catch(PDOException $e){
     echo "Error: " . $e->getMessage();
